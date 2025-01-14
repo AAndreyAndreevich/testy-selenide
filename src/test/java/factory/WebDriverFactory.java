dabfef2456bac2ad0685892437a5ac02cd187af0
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2025-01-14 22:03:22 +0300

    0.0

--- src/test/java/factory/WebDriverFactory.java
+++ src/test/java/factory/WebDriverFactory.java
@@ -4,10 +4,10 @@ public class WebDriverFactory {
     public static void setWebDriver(String driver) {
         switch (driver) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
                 break;
             case "edge":
-                System.setProperty("webdriver.edge.verboseLogging", "src\\test\\resources\\msedgedriver.exe");
+                System.setProperty("webdriver.edge.verboseLogging", "src\\test\\resources\\drivers\\msedgedriver.exe");
                 break;
             default:
                 break;
