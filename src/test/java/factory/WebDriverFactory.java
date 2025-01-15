b926b19513af377cc1af7125934609e22b6c856d
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2025-01-15 21:18:12 +0300

    0.0

--- src/test/java/factory/WebDriverFactory.java
+++ src/test/java/factory/WebDriverFactory.java
@@ -6,17 +6,14 @@ import enums.EnumDriver;
 public class WebDriverFactory {
     public static void setWebDriver(EnumDriver driver) {
         switch (driver) {
-            case CHROME:
+            case CHROME -> {
                 System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
                 Configuration.browser = "chrome";
-                break;
-            case EDGE:
+            }
+            case EDGE -> {
                 System.setProperty("webdriver.edge.verboseLogging", "src\\test\\resources\\drivers\\msedgedriver.exe");
                 Configuration.browser = "edge";
-                break;
-            default:
-                System.err.println("Такого драйвера нет, выберите 'edge' или 'chrome'");
-                System.exit(1);
+            }
         }
     }
 }
